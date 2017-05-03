a5723fcca8cad4e2da377b6486e453d39d437e91
Howard Liu <howard@ixnet.work>
2017-05-03 20:45:39 +0800

    Added Tool::getIP for obtaining client IP

--- Lib/Tool.php
+++ Lib/Tool.php
@@ -34,4 +34,15 @@ class Tool
             return $defult;
         }
     }
+
+    public static function getIP()
+    {
+        if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+            return $_SERVER['HTTP_CLIENT_IP'];
+        } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+            return $_SERVER['HTTP_X_FORWARDED_FOR'];
+        } else {
+            return $_SERVER['REMOTE_ADDR'];
+        }
+    }
 }
