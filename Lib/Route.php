a443a5ab522ba61c5d02e5ddf4e8d2798661e706
Howard Liu <howard@ixnet.work>
2017-05-06 10:48:08 +0800

    Rewritten function compactible with php integrated server

--- Lib/Route.php
+++ Lib/Route.php
@@ -15,7 +15,15 @@ class Route
     public function __construct()
     {
         $this->method = strtolower($_SERVER['REQUEST_METHOD']);
-        $this->action = $_GET['action'] = (isset($_GET['action']) && $_GET['action']) ? $_GET['action'] : 'index';
+        if (isset($_GET['action']) && $_GET['action'] !== '') {
+            $this->action = $_GET['action'];
+        } elseif ($_SERVER['REQUEST_URI'] != '/' &&
+            strpos($_SERVER['REQUEST_URI'], '.php') === false){
+            // If .php is not in REQUEST_URI, the request should have been rewritten
+            $this->action = substr($_SERVER['REQUEST_URI'], 1);
+        } else {
+            $this->action = 'index';
+        }
     }
 
     /*
