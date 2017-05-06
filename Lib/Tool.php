2c6ceee5af87a7a29fbe6fa03745d25ec7fab0dd
Howard Liu <howard@ixnet.work>
2017-05-06 12:24:04 +0800

    Separated randomHex function

--- Lib/Tool.php
+++ Lib/Tool.php
@@ -8,16 +8,29 @@ namespace Lib;
 
 class Tool
 {
+    /*
+     * Generate random HEX string
+     * $length is an integer in bytes, 1 byte = 2 hex digits
+     */
+    public static function randomHex($length = 16) {
+        if (function_exists('random_bytes')) {
+            return bin2hex(random_bytes($length));
+        } elseif (function_exists('openssl_random_pseudo_bytes')) {
+            return openssl_random_pseudo_bytes($length);
+        } else {
+            if ($length > 16) {
+                $randomHex = self::randomHex($length-16);
+            } else {
+                $randomHex = '';
+            }
+            return $randomHex.substr(md5(str_shuffle('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')), 0, $length*2);
+        }
+    }
+
     public static function hash($password, $randomString = '')
     {
         if (!$randomString) {
-            if (function_exists('random_bytes')) {
-                $randomString = bin2hex(random_bytes(16));
-            } elseif (function_exists('openssl_random_pseudo_bytes')) {
-                $randomString = openssl_random_pseudo_bytes(16);
-            } else {
-                $randomString = md5(str_shuffle('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'));
-            }
+            $randomString = self::randomHex();
         }
         $hashString  = $randomString.$password.Config::get('salt');
         return array(
