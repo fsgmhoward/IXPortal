e7f8aacb8352224bc06f0ef73878de52a3e4a589
Howard Liu <howard@ixnet.work>
2017-05-27 16:47:21 +0800

    Improved Request class

--- Lib/Request.php
+++ Lib/Request.php
@@ -8,19 +8,37 @@ namespace Lib;
 
 class Request
 {
-    public static function getPost($name, $default = null)
+    public static function getPost($name = null, $default = null)
     {
-        if (isset($_POST[$name]) && $_POST[$name] != '') {
-            return str_ireplace(['\'', '"'], '', $_POST[$name]);
+        if ($name) {
+            if (isset($_POST[$name]) && $_POST[$name] != '') {
+                return str_ireplace(['\'', '"'], '', $_POST[$name]);
+            } else {
+                return $default;
+            }
+        } elseif (!empty($_POST)) {
+            foreach ($_POST as $name => $value) {
+                $_POST[$name] = str_ireplace(['\'', '"'], '', $value);
+            }
+            return $_POST;
         } else {
             return $default;
         }
     }
 
-    public static function getRequest($name, $default = null)
+    public static function getRequest($name = null, $default = null)
     {
-        if (isset($_REQUEST[$name]) && $_REQUEST[$name] != '') {
-            return str_ireplace(['\'', '"'], '', $_REQUEST[$name]);
+        if ($name) {
+            if (isset($_REQUEST[$name]) && $_REQUEST[$name] != '') {
+                return str_ireplace(['\'', '"'], '', $_REQUEST[$name]);
+            } else {
+                return $default;
+            }
+        } elseif (!empty($_REQUEST)) {
+            foreach ($_REQUEST as $name => $value) {
+                $_REQUEST[$name] = str_ireplace(['\'', '"'], '', $value);
+            }
+            return $_REQUEST;
         } else {
             return $default;
         }
